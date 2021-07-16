aadd76cf7df167c99e3b2b3fa33dd82ed53f99e0
hlib <hlibbabii@gmail.com>
2021-07-16 16:52:24 +0200

    quit refactoring-miner process on timeout

--- commitexplorer/tools/refactoring_miner.py
+++ commitexplorer/tools/refactoring_miner.py
@@ -2,10 +2,11 @@ import subprocess
 import tempfile
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List, Dict, Tuple, Generator, Any
+from typing import List, Dict, Tuple, Generator, Any, Optional
 
 import jsons as jsons
 from git.objects import commit
+from jsons import DecodeError
 from tqdm import tqdm
 
 from commitexplorer import project_root
@@ -57,21 +58,28 @@ class RefactoringMiner(Tool):
             print(f'{type(self).__name__}: not a java project, skipping ...')
         else:
             commit_chunk = 1000
+            max_seconds_per_commit = 6
             for older_commit, newer_commit in tqdm(commit_boundary_generator(all_shas_new_to_old, commit_chunk)):
-                commit_result = self._run_on_commit_range(older_commit, newer_commit, path)
+                commit_result = self._run_on_commit_range(older_commit, newer_commit, path, commit_chunk * max_seconds_per_commit)
                 yield commit_result
 
-    def _run_on_commit_range(self, older_commit: commit.Commit, newer_commit: commit.Commit, path: Path) -> Dict[Sha, List]: #TODO run on commit also for sstubs?
+    def _run_on_commit_range(self, older_commit: commit.Commit, newer_commit: commit.Commit, path: Path, timeout: Optional[int] = None) -> Dict[Sha, List]: #TODO run on commit also for sstubs?
         with tempfile.NamedTemporaryFile() as f:
             print(f'Processing commits from {older_commit.hexsha} ({older_commit.committed_datetime.strftime("%Y-%m-%d %H:%M:%S")}) to {newer_commit.hexsha} ({newer_commit.committed_datetime.strftime("%Y-%m-%d %H:%M:%S")}) ...')
             cmd = ["./RefactoringMiner", "-bc", str(path), older_commit.hexsha, newer_commit.hexsha, '-json', f.name]
             print(f'Running command {cmd}')
-            subprocess.run(cmd, cwd=self.path, capture_output=True, check=True)
-            output: RefactoringMinerOutput = jsons.loads(f.read(), RefactoringMinerOutput)
-            n_commits_analyzed = len(output.commits)
-            dct = {c.sha1: c.refactorings for c in output.commits if c.refactorings}
-            n_refactorings = len(dct.items())
-            print(f'Refactorings detected: {n_refactorings}/{n_commits_analyzed}')
+            try:
+                subprocess.run(cmd, cwd=self.path, capture_output=True, check=True, timeout=timeout)
+            except subprocess.TimeoutExpired:
+                print(f"Warning: process {cmd} was interrupted because of the timeout.")
+            try:
+                output: RefactoringMinerOutput = jsons.loads(f.read(), RefactoringMinerOutput)
+                n_commits_analyzed = len(output.commits)
+                dct = {c.sha1: c.refactorings for c in output.commits if c.refactorings}
+                n_refactorings = len(dct.items())
+                print(f'Refactorings detected: {n_refactorings}/{n_commits_analyzed}')
+            except DecodeError:
+                dct = {}
             return dct
 
     def run_on_commit(self, commit: Commit) -> List:
