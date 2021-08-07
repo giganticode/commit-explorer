5f728da2b0d01f9971a088c0dda115d41e08a45c
hlib <hlibbabii@gmail.com>
2021-08-07 16:39:23 +0200

    migrate data: fixes

--- commitexplorer/migrate.py
+++ commitexplorer/migrate.py
@@ -12,7 +12,7 @@ for dirpath, dirnames, filenames in os.walk(storage_path):
         path = Path(dirpath) / file
         with path.open() as f:
             dct = json.loads(f.read())
-            sha = path.parent + path.parent.parent + path.name
+            sha = str(path.parent) + str(path.parent.parent) + str(path.name)
             if len(sha) != 40:
                 raise AssertionError(sha)
             db.commits.insert_one({'_is': sha, **dct})
