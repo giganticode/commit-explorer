733b39a0e154d356fabaa19bf7a72ea9d8442bc8
hlib <hlibbabii@gmail.com>
2021-09-02 11:46:53 +0200

    fix: write bytes instead of python objects

--- commitexplorer/server.py
+++ commitexplorer/server.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
 import http.server
 import socketserver
 import re
+import json
 
 from pymongo import MongoClient
 
@@ -17,25 +17,33 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, request, client_address, server, directory=None):
         super().__init__(request, client_address, server)
 
+    def send200(self, payload):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        payload_bytes = json.dumps(payload).encode('utf-8')
+        self.wfile.write(payload_bytes)
+
+    def handle_commit(self):
+        sha = self.path[11:]
+        if not sha_regex.fullmatch(sha):
+            return self.send_error(400, f'Invalid commit hashsum: {sha}')
+        commit = commit_collection.find_one({'_id': sha})
+        if commit is not None:
+            self.send200(commit)
+        else:
+            return self.send_error(404, f"Commit {sha} not found")
+
+    def handle_query(self):
+        ids = [sha['_id'] for sha in commit_collection.find({'bohr.200k_commits': {"$exists": True}}, {"_id": 1})]
+        print(f'Found {len(ids)} commits satisfying the query')
+        self.send200(ids)
+
     def do_GET(self):
         if self.path.startswith('/ce/commit/'):
-            sha = self.path[11:]
-            if not sha_regex.fullmatch(sha):
-                return self.send_error(400, f'Invalid commit hashsum: {sha}')
-            commit = commit_collection.find_one({'_id': sha})
-            if commit is not None:
-                self.send_response(200)
-                self.send_header('Content-Type', 'application/json')
-                self.end_headers()
-                self.wfile.write(commit)
-            else:
-                return self.send_error(404, f"Commit {sha} not found")
-        if self.path.startswith('/ce/query/'):
-            ids = [sha['_id'] for sha in commit_collection.find({'bohr.200k_commits': {"$exists": True}}, {"_id": 1})]
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-            self.wfile.write(ids)
+            self.handle_commit()
+        elif self.path.startswith('/ce/query/'):
+            self.handle_query()
         else:
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
